e2ba7e6f28d55bbc707139c87eb5e5e6ec71fe85
Cameron Fredrickson <cfredric@e1z2r2p10.42.us.org>
2017-02-26 22:22:16 -0800

    still have no clue how to fetch the token

--- ceryneian_hind.py
+++ ceryneian_hind.py
@@ -1,4 +1,6 @@
 from urllib2 import urlopen, Request
+import requests
+from requests_oauthlib import OAuth2, OAuth2Session
 import sys
 import requests
 import json
@@ -27,16 +29,19 @@ def api_call(uid, secret, auth_url):
 def main():
   UID = '45d33b9302c364a2130aa74e735459e64cfc4da181b37e9bb66160d7b09fed47'
   SECRET = '03a5508f94b6a7eb05ebec96642598f22001a35ca0f3959f9903bef194187519'
-  REDIRECT = 'https://api.intra.42.fr/oauth/authorize?client_id=45d33b9302c364a2130aa74e735459e64cfc4da181b37e9bb66160d7b09fed47&redirect_uri=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FOAuth&response_type=code'
+  REDIRECT_URI = 'https://api.intra.42.fr/oauth/authorize?client_id=45d33b9302c364a2130aa74e735459e64cfc4da181b37e9bb66160d7b09fed47&redirect_uri=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FOAuth&response_type=code'
   AUTHORIZE_URL = 'https://api.intra.42.fr/oauth/authorize?client_id=45d33b9302c364a2130aa74e735459e64cfc4da181b37e9bb66160d7b09fed47&redirect_uri=https%3A%2F%2Fen.wikipedia.org%2Fwiki%2FOAuth&scope=public&state=a_very_long_random_string_witchmust_be_unguessable'
-
-  f_user = sys.argv[1]
-  if not f_user:
+  SCOPE = "public"
+  try:
+    f_user = sys.argv[1]
+  except IndexError:
     print "A text file must be passed in as an argument when script.py is run."
-    print "usage: python script.py [--user file]"
+    print "\n\tusage: python script.py [--user file]\n"
     sys.exit(1)
-  api_call(UID, SECRET, AUTHORIZE_URL)
-  # print_user_locations(store_users(f_user))
+
+  oauth = OAuth2Session(UID, redirect_uri=REDIRECT_URI)
+  authorization_url, state = oauth.authorization_url("https://api.intra.42.fr/oauth/authorize")
+  token = oauth.fetch_token(SECRET, code=UID)
 
 if __name__ == '__main__':
   main()
